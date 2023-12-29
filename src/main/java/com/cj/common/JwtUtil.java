b998dba8d8073082b01f7f225082574bc6783e8b
chenjie <18822126754@163.com>
2023-12-29 18:23:44 +0800

    fix(jwt): jwt封装

---
+++ src/main/java/com/cj/common/JwtUtil.java
@@ -0,0 +1,149 @@
+package com.cj.common;
+
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.cj.entity.User;
+import com.cj.service.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.Resource;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class JwtUtil {
+
+    @Resource
+    private static UserService userService;
+
+
+    private  static final Long TOKEN_EXPIRED_TIME = 30*24*60*60L;
+
+    private static final String jwtId = "tokenId";
+
+    /**
+     * jwt加密解密密钥
+     */
+    private static final String JWT_SECRET = "jwt+00";
+
+
+    /**
+     * 创建jwt token
+     */
+    public static String createToken(Map<String, Object> claims,Long time){
+        SignatureAlgorithm hs256 = SignatureAlgorithm.HS256;  // 指定签名的时候使用签名算法，也就是header那部分
+        Date now = new Date(System.currentTimeMillis());
+
+
+        SecretKey secretKey = generalKey();
+
+        long nowMillis = System.currentTimeMillis();  // 生成jwt时间
+
+        // 下面就是在为payload添加各种标准声明和私有声明了
+        JwtBuilder builder = Jwts.builder()   // 这里其实就是new 一个JwtBuilder,设置jwt的body
+                .setClaims(claims)   // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的
+                .setId(jwtId)        // 设置jwt的id
+                .setIssuedAt(now)  // jwt的签发时间
+                .signWith(hs256, secretKey);  // 设置签名使用的签名算法和签名使用的秘钥
+
+        if (time >= 0) {
+            long expMillis = nowMillis + time;
+            Date exp = new Date((expMillis));
+            builder.setExpiration(exp);     // 设置过期时间
+        }
+
+        return builder.compact();
+    }
+
+
+    /**
+     * 验证jwt
+     */
+    public static Claims verifyJwt(String token) {
+        // 签名密钥，和生成签名的密钥一摸一样
+        SecretKey secretKey = generalKey();
+
+        Claims claims;
+
+        try{
+            claims = Jwts.parser()  // 得到DefaultJwtParser
+                    .setSigningKey(secretKey)        // 设置签名的秘钥
+                    .parseClaimsJws(token).getBody();
+        }catch (Exception e){
+            claims = null;
+        }
+
+        return claims;
+    }
+
+    /**
+     * 生成签名秘钥
+     */
+    public static SecretKey generalKey() {
+        String stringKey = JWT_SECRET;        // JWT密钥
+        byte[] encodedKey = Base64.getEncoder().encode(stringKey.getBytes());
+        SecretKeySpec spec = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
+        return spec;
+    }
+
+
+    /**
+     * 根据userId和openid生成token
+     *
+     */
+    public static String generateToken(User user) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", user.getId());
+        map.put("userName", user.getUsername());
+        return createToken(map, TOKEN_EXPIRED_TIME);
+    }
+
+
+    /**
+     * 获取当前用户的信息
+     */
+    public static User getCurrentUserInfo() {
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String token = request.getHeader("token");
+            if(StringUtils.isNotBlank(token)){
+                Claims claims = verifyJwt(token);
+                if (claims != null) {
+                    String userId = (String) claims.get("userId");
+                    return
+                            userService.getById(userId);
+                } else{
+                    return null;
+                }
+            }
+        }catch(Exception e){
+            return null;
+        }
+        return null;
+    }
+
+    /**
+     * 通过token获取用户id
+     */
+    public static Long getUserId(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Claims claims = verifyJwt(token);
+        if (claims != null){
+            return (Long) claims.get("id");
+        } else{
+            return 0L;
+        }
+    }
+
+
+}
--- src/test/java/com/cj/SpringBootProjectApplicationTests.java
+++ src/test/java/com/cj/SpringBootProjectApplicationTests.java
@@ -1,7 +1,10 @@
 package com.cj;
 
+import com.cj.common.JwtUtil;
 import com.cj.entity.Role;
+import com.cj.entity.User;
 import com.cj.mapper.RoleMapper;
+import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +22,24 @@ class SpringBootProjectApplicationTests {
         role.setRolename("副校长");
         role.setRemark("临时的");
         roleMapper.insert(role);
+    }
+
+    @Test
+    public void testJwt() {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("ceshi token");
+        String s = JwtUtil.generateToken(user);
+        //eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyTmFtZSI6ImNlc2hpIHRva2VuIiwiZXhwIjoxNzAzODQ3Njk4LCJ1c2VySWQiOjEsImlhdCI6MTcwMzg0NTEwNiwianRpIjoidG9rZW5JZCJ9.RN2umVnHbOIblf6HfvELpTJq-enhIgAx2KAwQp3jRcU
+        System.out.println(s);
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyTmFtZSI6ImNlc2hpIHRva2VuIiwiZXhwIjoxNzAzODQ3Njk4LCJ1c2VySWQiOjEsImlhdCI6MTcwMzg0NTEwNiwianRpIjoidG9rZW5JZCJ9.RN2umVnHbOIblf6HfvELpTJq-enhIgAx2KAwQp3jRcU";
+        // 验证jwt
+        Claims claims = JwtUtil.verifyJwt(token);
+
+        System.out.println(claims.get("userId"));
+        System.out.println(claims.get("userName"));
+
+        System.out.println(claims.get("id"));
     }
 
 }
